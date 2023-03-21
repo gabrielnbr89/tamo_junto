80b8061914e784384d5a52ee11f43efbf3b1a7fd
Gabriel Barrionuevo <gabrielnbr89@gmail.com>
2023-03-20 21:19:42 -0300

    agregando imagenes al home y ordenando. Footer

--- frontend/src/componentes/layout/Footer.js
+++ frontend/src/componentes/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = (props) => {
     return (
         <footer className='holder'>
-            <h3>Este es el pie de página - &copy; 2023</h3>
+            <h3>&copy; 2023 - TMJND Todos os direitos reservados.</h3>
         </footer>
     );
 }
--- frontend/src/paginas/HomePage.js
+++ frontend/src/paginas/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+// import { Carousel } from 'react-responsive-carousel';
 import Counter from '../componentes/animations/Counter.js';
 
 
@@ -31,34 +31,35 @@ const HomePage = (props) => {
             <hr />
             <div className="fila">
                 <div className="columna33">
-                    <img src="https://via.placeholder.com/280" alt="foto_teleconsulta" />
-                    <h3>Tecnologia inovadora</h3>
+                    <img src="img/tecnologia_inovadora.png" alt="foto_teleconsulta" />
+                    {/* <h3>Tecnologia inovadora</h3> */}
                     <p>Novas tecnologias para solucionar de forma inovadora e escalável desafios de prevenção e promoção à saúde.</p>
                     <a>Teleconsulta</a><br/>
                     <a>Política Privacidade dados</a>
                 </div>
                 <div className="columna33">
-                    <img src="https://via.placeholder.com/280" alt="foto_associados" />
-                    <h3>Projetos e Sub-projetos</h3>
+                    <img src="img/projetos_subprojetos.png" alt="projetos_subprojetos" />
+                    {/* <h3>Projetos e Sub-projetos</h3> */}
                     <p>Cuidando do meu diabetes com uma pequena ajuda dos meus amigos.</p>
                     <a>Projetos e Sub-projetos</a><br/>
                     <a href="/stakeholders">Stakeholders</a>
                 </div>
                 <div className="columna33">
-                    <img src="https://via.placeholder.com/280" alt="foto_noticias" />
-                    <h3>Gestão do cuidado</h3>
+                    <img src="img/gestao_cuidado.png" alt="gestao_cuidado" />
+                    {/* <h3>Gestão do cuidado</h3> */}
                     <p>Uma adequada GESTÃO da doença como forma para manter o sistema funcionando em harmonia.</p>
                     <a>Gestão gcdc</a><br/>
                     <a>Atualizações</a><br/>
                     <a>Transparencia</a>
                 </div>
             </div>
-            <hr />
+            
             <div className="socios">           
                     <Counter/>
             </div>
+            <hr />
             <section>
-                <h2>Nossos associados</h2>
+                <h2>NOSSOS PARCEIROS</h2>
                 <div className="fila">
                     <div className="columna100 socios">
                         <img src="https://via.placeholder.com/150" alt="foto_noticias" />
